fd3a03cce9377cbcb20448a68497d5df460e782d
brett ohland <brettohland@gmail.com>
2015-02-28 21:31:56 -0500

    Initial Commit

---
+++ SLCKRGF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SLCKRGF
+//
+//  Created by brett ohland on 2015-02-28.
+//  Copyright (c) 2015 ampersandsoftworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SLCKRGFTests/SLCKRGFTests.swift
@@ -0,0 +1,36 @@
+//
+//  SLCKRGFTests.swift
+//  SLCKRGFTests
+//
+//  Created by brett ohland on 2015-02-28.
+//  Copyright (c) 2015 ampersandsoftworks. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SLCKRGFTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
