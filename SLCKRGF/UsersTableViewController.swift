14650b67f7861e4ca76f34d200c5b9927eeba0aa
brett ohland <brettohland@gmail.com>
2015-03-02 21:33:38 -0500

    Baseline project

--- SLCKRGF/UsersTableViewController.swift
+++ SLCKRGF/UsersTableViewController.swift
@@ -10,44 +10,118 @@ import UIKit
 
 class UsersTableViewController: UITableViewController {
 
+    private var users: NSArray?
+    private var slackToken: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+        // Make a request to https://slack.com/api/rtm.start?token=xxxxxxxx&pretty=1
+        // Gets everything we need.
+        
+        // Get the token from the keys.plist
+        let plistPath = NSBundle.mainBundle().pathForResource("keys", ofType: "plist")
+        
+        if let path = plistPath {
+            if let key = NSDictionary(contentsOfFile: path) {
+                if let token = key["Slack API token"] as? String {
+                    slackToken = token
+                    getUsersWithToken(token)
+                } else {
+                    println("No 'Slack API Token' key in the keys.plist file, did you add it?")
+                }
+            } else {
+                // Key doesn't exist.
+                println("Can't find keys.plist, did you create and add a keys.plist file?")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Custom methods
+    
+    func getUsersWithToken(token:String) {
+        
+        refreshControl?.beginRefreshing()
+        
+        let slackURL = NSURL(string: "https://slack.com/api/rtm.start?token=\(token)&pretty=1")
+        let session = NSURLSession.sharedSession()
+        
+        if let url = NSURL(string: "https://slack.com/api/rtm.start?token=\(token)&pretty=1") {
+            
+            session.dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+                
+                let httpResponse = response as! NSHTTPURLResponse
+                let responseData = data as NSData
+                
+                if (error == nil) {
+                    
+                    if ( httpResponse.statusCode == 200 ){
+                        var jsonError: NSError?
+                        
+                        if let responseObject:NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as? NSDictionary {
+                            if let users = responseObject.objectForKey("users") as? NSArray {
+                                self.refreshControl?.endRefreshing()
+                                self.users = users
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                        if let theJSONError = jsonError {
+                            let reason : String? = theJSONError.localizedDescription
+                            println("Error decoing JSON: \(reason)")
+                        }
+                        
+                    } else {
+                        println("Returned HTTP code: \(httpResponse.statusCode)")
+                        println("Raw response: \(response)")
+                        
+                    }
+                } else {
+                    let reason : String? = error.localizedDescription
+                    println("Error reading from URL \(url) : \(reason)")
+                }
+                
+                
+            }).resume()
+        }
+        
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        
+        return users?.count ?? 0
+        
     }
 
-    /*
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("UserCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        if let currentUser = users?[indexPath.row] as? NSDictionary{
+            cell.textLabel?.text = currentUser["name"] as? String ?? ""
+        }
+        
         return cell
     }
-    */
+
+    @IBAction func refresh(sender: AnyObject) {
+        if let token = slackToken {
+            getUsersWithToken(token)
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
